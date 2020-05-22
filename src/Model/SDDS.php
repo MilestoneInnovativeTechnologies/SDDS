018f2566c1058bd4c810b2ed5eccee5503e38b75
Firose <firumon@github.com>
2020-05-22 16:20:42 +0530

    Model event added for clearing cache for domains

--- src/Model/SDDS.php
+++ src/Model/SDDS.php
@@ -3,9 +3,15 @@
 namespace Milestone\SDDS\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class SDDS extends Model
 {
     protected $table = 'sdds';
     protected $guarded = [];
+
+    protected static function booted()
+    {
+        static::saved(function(){ Cache::forget(config('sdds.cache')); });
+    }
 }
